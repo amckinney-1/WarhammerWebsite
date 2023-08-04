9a823437086e1877eee3bbde1894d118b6bf73c7
AndrewMc8 <78245384+AndrewMc8@users.noreply.github.com>
2023-08-04 08:49:42 -0600

    Dbconnector created

---
+++ dbConnector.php
@@ -0,0 +1,19 @@
+<?php
+
+//constants
+DEFINE("DB_USER", "root");
+DEFINE("DB_PSWD ", "");
+DEFINE("DB_SERVER", "localhost:3306");
+DEFINE("DB_NAME", "warhammerdb");
+
+//get things
+function ConnectionGet()
+{
+    $dbConn = @mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME, 3306)
+    OR die('Failed to connect to MySQL ' . DB_SERVER . '::' . DB_NAME . ' : ' . mysqli_connect_error()); // Display messge and end PHP script
+
+    return $dbConn;
+}
+
+function 
+?>
