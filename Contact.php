44023a8774ba15ca7b95ac7ddec0ea6aa39b14d6
AndrewMc8 <78245384+AndrewMc8@users.noreply.github.com>
2023-08-09 23:00:04 -0600

    Contact

---
+++ Contact.php
@@ -0,0 +1,16 @@
+<?php
+include_once "Header.php"
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>
+            Functional Fast Food Stacksburger
+        </h1>
+        <h2>
+            Andrew McFall <br /> Brandon Mann <br /> Jared Laing <br /> Austin McKinney
+        </h2>
+    </body>
+</html>
--- Navigation.php
+++ Navigation.php
@@ -2,4 +2,4 @@
 
 ?>
 <a href="Index.php">Home</a> &nbsp; &nbsp;<a href="ViewDatabase.php">View Database</a>&nbsp; &nbsp;<a href="ManagePages.php">Manage Pages</a>
-&nbsp; &nbsp;<a href=".php">Login</a>
+&nbsp; &nbsp;<a href="Contact.php">Meet The Team</a> &nbsp; &nbsp;<a href=".php">Login</a>
