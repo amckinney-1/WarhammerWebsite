098479441347655174f8846c4f46f9b60996293d
MergaTog <79662912+MergaTog@users.noreply.github.com>
2023-08-09 19:21:14 -0700

    Buttons added, not working

--- Header.php
+++ Header.php
@@ -24,6 +24,8 @@ if ($_SESSION["isAdmin"] == 1) {
 }
 ?>
 </h2>
-
+<input type="button" name="Css1" value="Css1 Style Change" onclick="<link rel='stylesheet' href='myStyle1.css'"/>
+<input type="button" name="Css2" value="Css2 Style Change" onclick="<link rel='stylesheet' href='myStyle2.css'"/>
+<input type="button" name="Css3" value="Css3 Style Change" onclick="<link rel='stylesheet' href='myStyle3.css'"/>
 <br/>
 <br/>
