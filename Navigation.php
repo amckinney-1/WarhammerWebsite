11626216ea72a15bb714c4d4a05f9d50cb62aaf7
BMann-png <bmann@student.neumont.edu>
2023-08-25 13:16:13 -0600

    Second Session_Start() deleted

--- Navigation.php
+++ Navigation.php
@@ -1,6 +1,5 @@
 <a href="Index.php">Home</a> &nbsp; &nbsp;<a href="ViewDatabase.php">View Database</a> &nbsp; &nbsp;<a href="Contact.php">Meet The Team</a>
 <?php
-session_start();
 //Displays the ManagePages if the user is an admin
 if (isset($_COOKIE["Login"]) == true) {
     $login = $_COOKIE["Login"];
